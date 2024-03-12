18b37f5cd923e0ef1ce137624b1a16c2e669547c
LitllePig <163136810+LitllePig@users.noreply.github.com>
2024-03-12 09:40:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://tr.rbxcdn.com/705c8865c5667d4b4e67c97819ae8a46/420/420/Image/Png";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://images-americanas.b2w.io/produtos/5898765922/imagens/pelucia-personagem-pou-jogo-infantil/5898765922_2_large.jpg";
+}
+
